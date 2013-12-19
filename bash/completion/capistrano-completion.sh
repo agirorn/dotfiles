54aa0390c3df325b993726c66e814ab74f6071bc
Ægir Örn Símonarson <agirorn@gmail.com>
2013-12-19 13:30:55 +0000

    Replacing capistrano compleation with a new faster one.

--- bash/completion/capistrano-completion.sh
+++
@@ -1,9 +0,0 @@
-# command compleation for nown rake commands.
-
-export COMP_WORDBREAKS=${COMP_WORDBREAKS/\:/}
-
-_capistrano_complete() {
-  COMPREPLY=($(compgen -W "`cap -T | grep '#' |awk '{{print $2}}'`" -- ${COMP_WORDS[COMP_CWORD]}))
-  return 0
-}
-complete -o default -o nospace -F _capistrano_complete cap
