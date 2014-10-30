e7fdb8efdfa46a8f9d6ff072377ac39544188e65
Ægir Örn Símonarson <agir@mbl.is>
2014-10-30 13:00:35 +0000

    Addign support for pip version 1.1

--- bash/completion/pip-bash-completion.sh
+++ bash/completion/pip-bash-completion.sh
@@ -1,12 +1,18 @@
 _pip_completion() {
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
-    first="${COMP_WORDS[0]}"
+    pip="${COMP_WORDS[0]}"
 
-    commands=$($first --help | awk '/Commands\:/,/General Options\:/' | \
+    commands=$($pip --help | awk '/Commands\:/,/General Options\:/' | \
                \grep -E -o "^\s{2}\w+" | tr -d ' ')
-    opts=$($first --help | \grep -E -o "((-\w{1}|--(\w|-)*=?)){1,2}")
 
+    # Fallback for pip version 1.1
+    if [ -z "$commands" ]; then
+        commands=$($first --help | awk '/Commands available/,/Commands\:/' | \
+               \grep -E -o "^\s{2}\w+" | tr -d ' ')
+    fi
+
+    opts=$($pip --help | \grep -E -o "((-\w{1}|--(\w|-)*=?)){1,2}")
 
     if [ $COMP_CWORD == 1 ] ; then
         COMPREPLY=( $(compgen -W "${commands}" -- ${cur}) )
@@ -14,7 +20,7 @@ _pip_completion() {
     fi
 
     if [[ ${cur} == -* ]] ; then
-        local command_opts=$($first $prev --help | \
+        local command_opts=$($pip $prev --help | \
                              \grep -E -o "((-\w{1}|--(\w|-)*=?)){1,2}")
         COMPREPLY=( $(compgen -W "${command_opts}" -- ${cur}) )
         return 0
