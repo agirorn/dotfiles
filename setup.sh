793824dcecf65b33f6cfe613d660bbefd6e1addd
Ægir Örn Símonarson <agirorn@gmail.com>
2014-11-13 09:45:13 +0000

    Adding config fyrir pep8 command.

--- setup.sh
+++ setup.sh
@@ -15,7 +15,7 @@ dot_files_dir=~/dotfiles                    # dotfiles directory
 backup_dir=~/dotfiles_old             # old dotfiles backup directory
 
 # list of files/folders to symlink in homedir
-dot_files="profile bash vimrc vim tmux.conf irbrc gemrc pylintrc pythonrc"
+dot_files="profile bash vimrc vim tmux.conf irbrc gemrc pylintrc pythonrc config"
 
 ########## Banner
 echo '########################'
