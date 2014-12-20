1fe2f7d39983c6057734575a9914802983b9f1c7
Ægir Örn Símonarson <agirorn@gmail.com>
2014-12-20 11:40:51 +0000

    Better alias

--- my-zsh/plugins/postgres/postgres.plugin.zsh
+++ my-zsh/plugins/postgres/postgres.plugin.zsh
@@ -1,8 +1,8 @@
 # Aliases to control Postgres
 # Paths noted below are for Postgres installed via Homebrew on OSX
 
-alias ps_start='pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start'
-alias ps_stop='pg_ctl -D /usr/local/var/postgres stop -s -m fast'
-alias ps_restart='stoppost && sleep 1 && startpost'
-alias ps_reload='pg_ctl reload -D /usr/local/var/postgres -s'
-alias ps_status='pg_ctl status -D /usr/local/var/postgres -s'
+alias post_start='pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start'
+alias post_stop='pg_ctl -D /usr/local/var/postgres stop -s -m fast'
+alias post_restart='stoppost && sleep 1 && startpost'
+alias post_reload='pg_ctl reload -D /usr/local/var/postgres -s'
+alias post_status='pg_ctl status -D /usr/local/var/postgres -s'
