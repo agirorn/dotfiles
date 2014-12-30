b4df46406258c019615bf6513b2baf922039cc18
Ægir Örn Símonarson <agir@mbl.is>
2014-12-30 14:10:39 +0000

    Adding alias for iPhone Simulator.

---
+++ my-zsh/iphone_simulatore.zsh
@@ -0,0 +1,6 @@
+#
+# alias for the iPhone Simulator.
+#
+if [[ -d '/Applications/Xcode.app/Contents/Developer/Applications/iOS Simulator.app' ]]; then
+  alias iphone="open '/Applications/Xcode.app/Contents/Developer/Applications/iOS Simulator.app'"
+fi
