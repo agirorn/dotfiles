ae409725aac758f87c1c01c3e836a79619b0949b
Ægir Örn Símonarson <agir@mbl.is>
2015-01-02 12:28:21 +0000

    Adding a functiona to automate test running in python.

--- my-zsh/python.zsh
+++ my-zsh/python.zsh
@@ -15,3 +15,29 @@ fi
 if [[ -f /usr/local/bin/virtualenvwrapper.sh ]]; then
   source /usr/local/bin/virtualenvwrapper.sh
 fi
+
+function python_tests() {
+  # run tests for a python project in a infinite loop
+  # with a default 60 sec delay between runs.
+
+  delay=$1
+  if [ -z "$delay" ]; then
+    delay=60
+  fi
+
+  while :; do
+    clear
+    if which -s jasmine-ci >> /dev/null; then
+      jasmine-ci --browser phantomjs
+    fi
+
+    # Only if Jasmine's tests pass.
+    if [ $? -eq 0 ]; then
+      if which -s nosetests >> /dev/null ; then
+        SELENIUM_DRIVER=PhantomJS nosetests tests/
+      fi
+    fi
+
+    sleep $delay
+  done
+}
