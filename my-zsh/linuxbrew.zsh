a51c992a13e40cfb248fb4613fe44518205dfd24
Ægir Örn Símonarson <agirorn@novomaticls.com>
2015-10-14 09:05:34 +0000

    Add support for linuxbrew.

---
+++ my-zsh/linuxbrew.zsh
@@ -0,0 +1,8 @@
+# LinuxBrew
+#
+# https://github.com/Homebrew/linuxbrew
+if [ -d $HOME/.linuxbrew ]; then
+  export PATH="$HOME/.linuxbrew/bin:$PATH"
+  export MANPATH="$HOME/.linuxbrew/share/man:$MANPATH"
+  export INFOPATH="$HOME/.linuxbrew/share/info:$INFOPATH"
+fi
