7db934d370f9fbc6533c3be67211f5a914dbc2ff
Ægir Örn Símonarson <agirorn@novomaticls.com>
2016-04-13 10:35:51 +0000

    Add support for pipin in to atom editor withe atome alias

--- my-zsh/aliases.zsh
+++ my-zsh/aliases.zsh
@@ -5,6 +5,7 @@ alias wgs='while :; do clear && git status; sleep 1; done;'
 alias c='clear'
 alias kill-swap='find . -iname ".*.swp" -exec rm {} \;'
 alias remove-vim-swap='find . -iname '.*.swp' -exec rm {} \;'
+alias atome="tmpin atom"
 
 # run command for ever with 1 sec delay.
 #
