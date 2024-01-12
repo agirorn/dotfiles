1bf1fd6aa9b45e9e77849f6a7ef253e7949a7f91
Ægir Örn Símonarson <agirorn@gmail.com>
2024-01-12 16:14:12 +0000

    Adding more aliases

--- my-zsh/aliases.zsh
+++ my-zsh/aliases.zsh
@@ -67,6 +67,12 @@ test -d /usr/local/opt/zlib/li && \
 test -d /usr/local/opt/zlib/include && \
   export CPPFLAGS="-I/usr/local/opt/zlib/include -I/usr/local/opt/bzip2/include"
 
+# Print out the output hepler sutff when running dotnet tests
+alias dtest='dotnet test --logger "console;verbosity=detailed"'
+
+# unit time command and not the build in bash ot zsh one
+test -f /usr/bin/time && alias utime=/usr/bin/time
+
 # This is required by PYENV for seom strange reason the plugin can not just
 # figure this out
 # export ZSH_PYENV_QUIET=true
